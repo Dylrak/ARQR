534a063b55902c32f653264f21d259d8b908dfae
Dylan Rakiman <dylan.rakiman@student.hu.nl>
2019-05-16 16:40:15 +0200

    Added Null-code links to Square Detection and Projection Matrix Calculation to Transform3DImage script

---
+++ Assets/Scenes/RecognizeCorners.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Square
+{
+    public Vector2 upperLeft;
+    public Vector2 upperRight;
+    public Vector2 lowerLeft;
+    public Vector2 lowerRight;
+}
+
+public class RecognizeCorners : MonoBehaviour
+{
+    // Obtain a list of all the squares which can be detected in the camera input image
+    public void getSquares(out List<Square> detected_squares_list)
+    {
+        //Placeholder code:
+        detected_squares_list = new List<Square>();
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scenes/Transform3DModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transform3DModel : MonoBehaviour
+{
+    public GameObject webCamImage;
+    public GameObject eventSystem;
+
+    private RecognizeCorners recognizeCorners;
+    private CalculateProjectionMatrix calculateProjectionMatrix;
+
+    public List<Square> detected_squares_list;
+
+    void Awake()
+    {
+        webCamImage = GameObject.Find("WebCamImage");
+        eventSystem = GameObject.Find("EventSystem");
+        recognizeCorners = webCamImage.GetComponent<RecognizeCorners>();
+        calculateProjectionMatrix = eventSystem.GetComponent<CalculateProjectionMatrix>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(1f, 1f, 1f);
+        recognizeCorners.getSquares(out detected_squares_list);
+        foreach (Square square in detected_squares_list)
+        {
+
+        }
+    }
+}
